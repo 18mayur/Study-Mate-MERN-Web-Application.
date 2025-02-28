9e14c207967d6d639086f1dce706b3b328a9a13d
18mayur <mayu2003shinde@gmail.com>
2025-02-28 13:25:37 +0530

    port

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5174"],
+    origin: ["http://localhost:5173"],
     credentials: true,
     // methods: ("GET","POST")
   })
